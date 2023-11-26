49d897135d8a6fafc8434aa87faa8846bb51ebb9
NeliMasalova <nellymosolova@gmail.com>
2023-11-26 12:45:04 +0100

    task 3, task 4

--- src/main/java/pl/pingwit/lec_2/Task2_Parsing.java
+++ src/main/java/pl/pingwit/lec_2/Task2_Parsing.java
@@ -4,27 +4,27 @@ public class Task2_Parsing {
     public static void main(String[] args) {
         String myByteString ="77";
         Byte myParsedByte = Byte.parseByte(myByteString);
-        System.out.println(myParsedByte);
-        System.out.println(myParsedByte + 6);
+            System.out.println(myParsedByte);
+            System.out.println(myParsedByte + 6);
 
         String myShortString ="12587";
         Short myParsedShort = Short.parseShort(myShortString);
-        System.out.println(myParsedShort);
-        System.out.println(myParsedShort + 6);
+            System.out.println(myParsedShort);
+            System.out.println(myParsedShort + 6);
 
         String myIntString ="-78945627";
         Integer myParsedInteger = Integer.parseInt(myIntString);
-        System.out.println(myParsedInteger);
-        System.out.println(myParsedInteger + 6);
+            System.out.println(myParsedInteger);
+            System.out.println(myParsedInteger + 6);
 
         String myFloatString ="123.6789";
         Float myParsedFloat = Float.parseFloat(myFloatString);
-        System.out.println(myParsedFloat);
-        System.out.println(myParsedFloat + 6);
+            System.out.println(myParsedFloat);
+            System.out.println(myParsedFloat + 6);
 
         String myDoubleString ="-8965.876655";
         Double myParsedDouble = Double.parseDouble(myDoubleString);
-        System.out.println(myParsedDouble);
-        System.out.println(myParsedDouble + 6.5);
+            System.out.println(myParsedDouble);
+            System.out.println(myParsedDouble + 6.5);
     }
 }
---
+++ src/main/java/pl/pingwit/lec_2/Task3_addition.java
@@ -0,0 +1,20 @@
+package pl.pingwit.lec_2;
+
+public class Task3_addition {
+    public static void main(String[] args) {
+        /*int myInt = 1993;
+        int mySecondInt = 2020;
+        int sum = myInt + mySecondInt;
+            System.out.println(sum);*/
+
+        int myInt = 1993;
+        int mySecondInt = 2020;
+        mySecondInt += myInt;
+            System.out.println("TheSum= " + mySecondInt);
+
+        /*int myInt = 1993;
+        int mySecondInt = ++myInt;
+            System.out.println("mySecondInt= " + mySecondInt);*/
+
+    }
+}
