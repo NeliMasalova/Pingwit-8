f43e5661d4fd7cdee911f687fc079e9124f89a11
NeliMasalova <nellymosolova@gmail.com>
2023-11-26 16:49:11 +0100

    task 5 and 6

---
+++ src/main/java/pl/pingwit/lec_2/Task4_multiplication.java
@@ -0,0 +1,13 @@
+package pl.pingwit.lec_2;
+
+public class Task4_multiplication {
+    public static void main(String[] args) {
+        int myInt = 7;
+        int mySecondInt = 21;
+        int multiplication = mySecondInt * myInt;
+            System.out.println(multiplication);
+
+
+
+    }
+}
---
+++ src/main/java/pl/pingwit/lec_2/Task5_divisionRest.java
@@ -0,0 +1,10 @@
+package pl.pingwit.lec_2;
+
+public class Task5_divisionRest {
+    public static void main(String[] args) {
+        int myFirstInt = 21;
+        int mySecondInt = -10;
+        int devisionRest = myFirstInt % mySecondInt;
+            System.out.println(devisionRest);
+    }
+}
