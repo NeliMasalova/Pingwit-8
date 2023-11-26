0faced603671c4ae3fd7f3cc5aca7cfe04c75e5f
NeliMasalova <nellymosolova@gmail.com>
2023-11-26 11:03:45 +0100

    initial commit

---
+++ src/main/java/pl/pingwit/lec_1/HelloWorld.java
@@ -0,0 +1,8 @@
+package pl.pingwit.lec_1;
+
+public class HelloWorld {
+    public static void main(String[] args ) {
+        System.out.println( "Hello World, hello " + args[0] + args[1] );
+    }
+}
+
