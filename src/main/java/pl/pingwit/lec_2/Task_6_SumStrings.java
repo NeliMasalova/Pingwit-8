951422ea79444ba97613a766f0b840425dfce3b3
Alexander Fomin <RezviyBelorus@gmail.com>
2023-11-27 19:03:58 +0100

    Update Task_6_SumStrings.java

--- src/main/java/pl/pingwit/lec_2/Task_6_SumStrings.java
+++ src/main/java/pl/pingwit/lec_2/Task_6_SumStrings.java
@@ -2,6 +2,7 @@ package pl.pingwit.lec_2;
 
 public class Task_6_SumStrings {
     public static void main(String[] args) {
+        // удали эту строку
         String firstString = "1000";
         String secondString = "250";
         Integer myFirstParsedInteger = Integer.parseInt(firstString);
