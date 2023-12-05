427540f0eb88cf39b25d09e41e3ca0fc4223bc94
Alexander Fomin <RezviyBelorus@gmail.com>
2023-12-05 13:01:59 +0100

    Update Task_7_OddNumbers.java

--- src/main/java/pl/pingwit/lec_3/Task_7_OddNumbers.java
+++ src/main/java/pl/pingwit/lec_3/Task_7_OddNumbers.java
@@ -2,6 +2,7 @@ package pl.pingwit.lec_3;
 
 public class Task_7_OddNumbers {
     public static void main(String[] args) {
+        // тоже абсолютно верно, просто для себя одну из этих задач сделай с использованием оператора %
         int sumOddNumbers = 0;
         for (int oddNumber = 1; oddNumber < 100; oddNumber = oddNumber + 2) {
             sumOddNumbers += oddNumber;
