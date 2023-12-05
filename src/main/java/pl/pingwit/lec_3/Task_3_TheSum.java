f279b7f9cbb6e86617ee0185e9cbae79951e627e
Alexander Fomin <RezviyBelorus@gmail.com>
2023-12-05 12:56:07 +0100

    Update Task_3_TheSum.java

--- src/main/java/pl/pingwit/lec_3/Task_3_TheSum.java
+++ src/main/java/pl/pingwit/lec_3/Task_3_TheSum.java
@@ -2,10 +2,10 @@ package pl.pingwit.lec_3;
 
 public class Task_3_TheSum {
     public static void main(String[] args) {
-        int myInt = 0;
-        int myTotalInt = myInt;
+        int myInt = 0; // я думаю это переменную можно удалить
+        int myTotalInt = myInt; // int myTotalInt = 0;
         for (int i = 1; i <= 256; i = i * 2) {
-            myTotalInt += myInt + i;
+            myTotalInt += myInt + i; // myTotalInt += i;
         }
         System.out.println(myTotalInt);
     }
