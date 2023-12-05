7876305c259836061be5f6e4a877089e23e9e1df
Alexander Fomin <RezviyBelorus@gmail.com>
2023-12-05 13:31:25 +0100

    Update Task_2_CarInsurance.java

--- src/main/java/pl/pingwit/lec_4/Task_2_CarInsurance.java
+++ src/main/java/pl/pingwit/lec_4/Task_2_CarInsurance.java
@@ -1,9 +1,9 @@
 package pl.pingwit.lec_4;
 import java.util.Scanner;
-public class Task_2_CarInsurance {
-    public static final double ENGINESIZECOEFFICIENT = 0.25;
-    public static final double INCREASEDKOEFFICIENT = 1.2;
-    public static final double REDUCEDCOEFFICIENT = 0.8;
+public class Task_2_CarInsurance { // я бы предложил добавить абзац как в прошлой задаче. Можно не добавлять, а убрать в предыдущей. Главное чтобы стиль был одинаковый
+    public static final double ENGINESIZECOEFFICIENT = 0.25; // название констант через _
+    public static final double INCREASEDKOEFFICIENT = 1.2; // название констант через _
+    public static final double REDUCEDCOEFFICIENT = 0.8; // название констант через _
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,19 +14,19 @@ public class Task_2_CarInsurance {
         System.out.println("Have you been insured with us earlier?");
         boolean ourInsurance = scanner.nextBoolean();
 
-        double insurancePolicy = engineSize * ENGINESIZECOEFFICIENT;
+        double insurancePolicy = engineSize * ENGINESIZECOEFFICIENT;// insurancePolicy - эта переменная хранит цену, поэтому лучше insurancePolicyPrice,
 
         if (carAccidents) {
             insurancePolicy = insurancePolicy * INCREASEDKOEFFICIENT;
-        } else {
+        } else { // этот else ничего не меняет, давай его просто уберем
             insurancePolicy = insurancePolicy;
         }
 
         if (ourInsurance) {
             insurancePolicy = insurancePolicy * REDUCEDCOEFFICIENT;
-        } else {
+        } else { // этот else ничего не меняет, давай его просто уберем
             insurancePolicy = insurancePolicy;
         }
-        System.out.println("YOUR INSURANCE POLICY WILL BE " + insurancePolicy + " euro.");
+        System.out.println("YOUR INSURANCE POLICY WILL BE " + insurancePolicy + " euro."); // а почему все предложение большими буквами, кроме валюты? Не угрожай клиентам)
     }
 }
