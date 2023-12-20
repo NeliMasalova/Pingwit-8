ce9b05156ee4cbea0f728a71add34fb94e9c8b85
Alexander Fomin <RezviyBelorus@gmail.com>
2023-12-20 09:31:37 +0100

    Update CarOwnerInformation.java

--- src/main/java/pl/pingwit/lec_6/Task_2_ParkingSystem/CarOwnerInformation.java
+++ src/main/java/pl/pingwit/lec_6/Task_2_ParkingSystem/CarOwnerInformation.java
@@ -1,10 +1,10 @@
 package pl.pingwit.lec_6.Task_2_ParkingSystem;
 
 public class CarOwnerInformation {
-    private String carOwnerName;
-    private String carOwnerSurname;
-    private String carOwnerTelephone;
-    private String carOwnerEmailAddress;
+    private String carOwnerName; // carOwnerName -> name
+    private String carOwnerSurname; //surname
+    private String carOwnerTelephone; //phone
+    private String carOwnerEmailAddress; // email
 
     public String getCarOwnerName() {
         return carOwnerName;
@@ -34,6 +34,7 @@ public class CarOwnerInformation {
         return carOwnerEmailAddress;
     }
 
+    // метод toString всегда в конце самом, после equals+hashcode (coming soon)
     @Override
     public String toString() {
         return "CarOwnerInformation{" +
