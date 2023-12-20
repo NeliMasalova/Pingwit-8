8e15ca71f497b0bcea79838076ebd2320e4a65f3
Alexander Fomin <RezviyBelorus@gmail.com>
2023-12-20 09:17:51 +0100

    Update Task_2_TheAverageArithmeticOfArray.java

--- src/main/java/pl/pingwit/lec_5/Task_2_TheAverageArithmeticOfArray.java
+++ src/main/java/pl/pingwit/lec_5/Task_2_TheAverageArithmeticOfArray.java
@@ -7,7 +7,7 @@ public class Task_2_TheAverageArithmeticOfArray {
         for (int v : myIntArray) {
             sum += v;
         }
-        double average = (double) sum / myIntArray.length;
+        double average = (double) sum / myIntArray.length; // вот это вычисление перенеси в блок else, а то получается если срабатывает if(), то мы не используем переменную. Давай еще уберем (double), он здесь не нужен
 
         if (myIntArray.length == 0) {
             System.out.println("Sorry, can't to count the average arithmetic of array, because the array is empty.");
