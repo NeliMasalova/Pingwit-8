9b706986becbad47c926a5bdd9367f5b02f25ff8
Alexander Fomin <RezviyBelorus@gmail.com>
2024-01-09 09:35:02 +0100

    Update Array.java

--- src/main/java/pl/pingwit/lec_7/Task_4_Array/Array.java
+++ src/main/java/pl/pingwit/lec_7/Task_4_Array/Array.java
@@ -3,14 +3,14 @@ package pl.pingwit.lec_7.Task_4_Array;
 import java.util.Arrays;
 
 public class Array {
-    public static final int CHANGED_CONDITION = 5;
+    public static final int CHANGED_CONDITION = 5; // CHANGED_CONDITION грозное название, думаю MULTIPLIER или EVEN_MULTIPLIER сюда лучше подойдет
 
     public static void main(String[] args) {
         int[] array = {-3, -2, 0, -7, -8, -9, -10};
         PrintArray(array);
     }
 
-    private static void PrintArray(int[] sourceArray) {
+    private static void PrintArray(int[] sourceArray) { // PrintArray - название метода с маленькой буквы
         int[] array = new int[sourceArray.length];
         for (int i = 0; i < sourceArray.length; i++) {
             if (sourceArray[i] % 2 == 0) {
