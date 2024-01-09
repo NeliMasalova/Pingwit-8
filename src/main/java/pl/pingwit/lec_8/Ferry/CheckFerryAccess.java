e4124339d341e918e11ab691e9d364b0d6704982
Alexander Fomin <RezviyBelorus@gmail.com>
2024-01-09 09:46:28 +0100

    Update CheckFerryAccess.java

--- src/main/java/pl/pingwit/lec_8/Ferry/CheckFerryAccess.java
+++ src/main/java/pl/pingwit/lec_8/Ferry/CheckFerryAccess.java
@@ -6,7 +6,7 @@ public class CheckFerryAccess {
     public static final int MAX_WEIGHT = 15;
 
     // в этом методе тебе необходимо посчитать всех пассажиров в машинах и общий вес авто. И затем принять решение в допуске или отказе. Ты пока проверяешь для каждого отдельного автомобиля
-    public static Boolean checkAccess(Vehicle[] vehicles) {
+    public static Boolean checkAccess(Vehicle[] vehicles) { // убери у метода static
         boolean isAllowed = true;
         for (int i = 0; i < vehicles.length; i++) {
             if (vehicles[i].getBoardPeople() <= MAX_PASSENGERS_NUMBER && vehicles[i].getWeight() < MAX_WEIGHT) {
