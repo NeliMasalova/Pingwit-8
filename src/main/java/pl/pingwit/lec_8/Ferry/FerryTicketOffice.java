4f46114c1ee16b91fec063e735945d26eea7568b
Alexander Fomin <RezviyBelorus@gmail.com>
2024-01-09 09:46:20 +0100

    Update FerryTicketOffice.java

--- src/main/java/pl/pingwit/lec_8/Ferry/FerryTicketOffice.java
+++ src/main/java/pl/pingwit/lec_8/Ferry/FerryTicketOffice.java
@@ -9,7 +9,7 @@ public class FerryTicketOffice {
     public static final BigDecimal MAX_PASSENGER_CAR_PRICE = BigDecimal.valueOf(15.0);
     public static final int NUMBER_OF_PASSENGERS = 2;
 
-    public static void calculateAmountPayment(Vehicle[] vehicles) {
+    public static void calculateAmountPayment(Vehicle[] vehicles) { // убери у метода static
         BigDecimal ticketPrice = BigDecimal.valueOf(0.0);
         BigDecimal amountPay = BigDecimal.valueOf(0.0);
         int passengers = 0;
