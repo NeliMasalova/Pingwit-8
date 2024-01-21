997a12e32aa4da3283a3eb5c0de0b24ce3559c68
Alexander Fomin <RezviyBelorus@gmail.com>
2024-01-21 09:53:30 +0100

    Update Task_2_palindrome.java

--- src/main/java/pl/pingwit/lec_8/Task_2_palindrome.java
+++ src/main/java/pl/pingwit/lec_8/Task_2_palindrome.java
@@ -2,12 +2,13 @@ package pl.pingwit.lec_8;
 
 import java.util.Scanner;
 
+// для этой задачи создай пакет task_2, а сам класс переименуй в Palindrome
 public class Task_2_palindrome {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter the line: ");
         String string = scanner.nextLine();
-        char[] array = string.toCharArray();
+        char[] array = string.toCharArray(); // давай попробуем удалить массив, а у переменной string вызвать метод charAt(сюда передаем i)
         boolean isPalindrome = false;
         for (int i = 0; i < array.length / 2; i++) {
             isPalindrome = array[i] == array[array.length - i - 1];
