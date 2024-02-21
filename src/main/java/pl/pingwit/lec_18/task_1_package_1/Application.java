1af752c42df2edf7e74d1ce1f14fbb93a83becd4
Alexander Fomin <RezviyBelorus@gmail.com>
2024-02-21 08:43:17 +0100

    Update Application.java

--- src/main/java/pl/pingwit/lec_18/task_1_package_1/Application.java
+++ src/main/java/pl/pingwit/lec_18/task_1_package_1/Application.java
@@ -14,6 +14,11 @@ public class Application {
         File file2 = new File(path2);
         System.out.println(file2.isDirectory());
 
+        /*
+        Давай попробуем усложнить эту часть, чтобы красиво было. Будем печатать на экран все файлы .txt, т.е.
+        можно у file2 вызвать метод listFiles() и через проверку из isFile() + isTxtFile() - это твой кастомный метод и затем класс сканнер.
+        У класса строк есть метод lastIndexOf("."), при помощи которого ты сможешь получить расширение файла -> в метод subString передать полученный индекс
+        */
         String path3 = "src/main/java/pl/pingwit/lec_18/task_1_package_1/package_2/example.txt";
         File file3 = new File(path3);
 
