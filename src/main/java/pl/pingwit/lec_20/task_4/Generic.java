05c2dee02df49fdba5f2056e20c39778da465b9b
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 20:40:28 +0100

    Update Generic.java

--- src/main/java/pl/pingwit/lec_20/task_4/Generic.java
+++ src/main/java/pl/pingwit/lec_20/task_4/Generic.java
@@ -1,6 +1,6 @@
 package pl.pingwit.lec_20.task_4;
 
-public class Generic<T> {
+public class Generic<T> { // Generic -> MyGeneric хотя бы, а лучше House или любое другое адекваное название или неадекватное, но другое, не используй зарезервированные названия
     private T value;
 
     public Generic(T value) {
