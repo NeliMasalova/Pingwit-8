b26dc0fbc849fa64ee701aa193fa76f1fc931f87
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 20:25:06 +0100

    Update Application.java

--- src/main/java/pl/pingwit/lec_19/task_1/Application.java
+++ src/main/java/pl/pingwit/lec_19/task_1/Application.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Application {
     public static void main(String[] args) throws IOException {
         Motorcycle motorcycle = new Motorcycle("Yamaha", "R1M", 998, "m");
-        XmlMapper xmlMapper = new XmlMapper();
+        XmlMapper xmlMapper = new XmlMapper(); // скорее всего здесь тебе нужен был ObjectOutputStream, тогда поле точно не будет сериализоваться. XmlMapper возможно не обращает внимания на то что полк transient
 
         try {
             xmlMapper.writeValue(new File("src/main/java/pl/pingwit/lec_19/task_1/motorcycle7.xml"), motorcycle);
