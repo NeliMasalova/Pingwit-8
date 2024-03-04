801a6f3ee16e1c45b5d52a4e722a57e38465533f
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-04 20:44:05 +0100

    Update GenericMain.java

--- src/main/java/pl/pingwit/lec_20/task_4/GenericMain.java
+++ src/main/java/pl/pingwit/lec_20/task_4/GenericMain.java
@@ -4,10 +4,11 @@ import java.math.BigDecimal;
 
 public class GenericMain {
     public static void main(String[] args) {
-        Generic<?> firstGeneric = new Generic<>("It's a generic string");
-        Generic<?> secondGeneric = new Generic<>(123789654L);
-        Generic<?> thirdGeneric = new Generic<>(new BigDecimal(10.58));
-        Generic<?> fourthGeneric = new Generic<>(true);
+        // с wild card (?) это ты сильно конечно, но этот механизм для другого
+        Generic<?> firstGeneric = new Generic<>("It's a generic string"); // Generic<?> -> Generic<String>
+        Generic<?> secondGeneric = new Generic<>(123789654L); // Generic<?> -> Generic<Long>
+        Generic<?> thirdGeneric = new Generic<>(new BigDecimal(10.58)); // Generic<?> -> Generic<BigDecimal>
+        Generic<?> fourthGeneric = new Generic<>(true); // угадай :)
 
         firstGeneric.printType();
         secondGeneric.printType();
