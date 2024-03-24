72493a620e81c4ddf9115dbca8d4adefacbdf96d
NeliMasalova <nellymosolova@gmail.com>
2024-03-24 20:05:31 +0100

    Lec 24.+ task 10

--- src/main/java/pl/pingwit/lec_24/task_10/LambdaComparator.java
+++ src/main/java/pl/pingwit/lec_24/task_10/LambdaComparator.java
@@ -6,18 +6,17 @@ import java.util.Comparator;
 public class LambdaComparator {
     public static void main(String[] args) {
         ArrayList<String> strings = new ArrayList<>();
-        strings.add("a");
+        strings.add("adfdf");
         strings.add("b");
-        strings.add("c");
-        strings.add("d");
+        strings.add("efqc");
+        strings.add("fq3rgrd");
         strings.add("e");
-        strings.add("f");
-        strings.add("g");
         strings.add("k");
 
-        Comparator<String> reverseComparator = (s1, s2) -> s2.compareTo(s1);
+        Comparator<String> reverseComparator = ((s1, s2) -> s2.compareTo(s1));
 
         strings.sort(reverseComparator);
+
         for (String str : strings) {
             System.out.println(str);
         }
