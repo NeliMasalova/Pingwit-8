b77e0120cc4da6e311daa2e7e1b459c1186b988d
NeliMasalova <nellymosolova@gmail.com>
2024-04-07 18:36:38 +0200

    lec_25,upgrade task_9

--- src/main/java/pl/pingwit/lec_24/task_9/PredicateExample.java
+++ src/main/java/pl/pingwit/lec_24/task_9/PredicateExample.java
@@ -1,30 +1,38 @@
 package pl.pingwit.lec_24.task_9;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateExample {
     public static void main(String[] args) {
 
-        List<UserInfo> userInfos = List.of(
-                new UserInfo("Sasha", "Petrov", "1234567", "sp@gmail.com", LocalDate.of(1990, 12, 12)),
-                new UserInfo("Pavel", "Ivanov", "987654", "pi@gmail.com", LocalDate.of(1990, 12, 12)),
-                new UserInfo("Anna", "Sidorova", "332211", "as@gmail.com", LocalDate.of(2005, 12, 12)),
-                new UserInfo("Lena", "Ivanova", "76555", "li@gmail.com", LocalDate.of(1998, 12, 12)),
-                new UserInfo("Sasha", "Sergeeev", "98876666", "ss@gmail.com", LocalDate.of(2005, 12, 12)));
+        List<UserInfo> userInfos = getUserInfos();
 
         // давай уберем "!"
-        // startsWithPredicate -> excludeNamesStartsWith
-        Predicate<UserInfo> startsWithPredicate = userInfo -> !userInfo.name().startsWith("S")
-                && !userInfo.name().startsWith("P")
-                && !userInfo.name().startsWith("A");
+        Predicate<UserInfo> excludeNamesStartsWith = userInfo -> userInfo.name().startsWith("S")
+                || userInfo.name().startsWith("P")
+                || userInfo.name().startsWith("A");
 
-        // userInfos.removeIf(startsWithPredicate); вместо кода ниже
-        List<UserInfo> infosResult = userInfos.stream()
-                .filter(startsWithPredicate)
-                .toList();
+        userInfos.removeIf(excludeNamesStartsWith);
+        System.out.println(userInfos);
+    }
 
-        infosResult.forEach(System.out::println);
+    private static List<UserInfo> getUserInfos() {
+        List<UserInfo> userInfos = new ArrayList<>();
+
+        UserInfo userSasha = new UserInfo("Sasha", "Petrov", "1234567", "sp@gmail.com", LocalDate.of(1990, 12, 12));
+        UserInfo userPavel = new UserInfo("Pavel", "Ivanov", "987654", "pi@gmail.com", LocalDate.of(1990, 12, 12));
+        UserInfo userAnna = new UserInfo("Anna", "Sidorova", "332211", "as@gmail.com", LocalDate.of(2005, 12, 12));
+        UserInfo userLena = new UserInfo("Lena", "Ivanova", "76555", "li@gmail.com", LocalDate.of(1998, 12, 12));
+        UserInfo userSashaS = new UserInfo("Sasha", "Sergeeev", "98876666", "ss@gmail.com", LocalDate.of(2005, 12, 12));
+
+        userInfos.add(userAnna);
+        userInfos.add(userLena);
+        userInfos.add(userPavel);
+        userInfos.add(userSashaS);
+        userInfos.add(userSasha);
+        return userInfos;
     }
 }
